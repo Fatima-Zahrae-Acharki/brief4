c198c34c97759b17ab9d9793998d80322cc7f6e8
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2022-11-23 15:29:24 +0100

    basic tp

---
+++ basic-tp/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('brief',[testController::class,'test1']);
+Route::get('brief',[testController::class,'select']);
+Route::delete('brief/{id}',[testController::class,'destroy']);
+Route::get('/brief/{id}', [testController::class,'edit']);
+Route::put('/brief/{id}', [testController::class,'update']);
