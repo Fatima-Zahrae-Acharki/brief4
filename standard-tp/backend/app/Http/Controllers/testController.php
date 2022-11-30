d36f802a5913e53a023bb804b748c71e4a012bb7
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2022-11-30 16:50:35 +0100

    standard

---
+++ standard-tp/backend/app/Http/Controllers/testController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\task;
+use Illuminate\Http\Request;
+
+class testController extends Controller
+{
+    //
+    public function test1(Request $request){
+        $brief = new task;
+        $brief->firstname = $request->firstname;
+        $brief->lastname = $request->lastname;
+        $brief->email = $request->email;
+        $brief->save();
+        return response()->json($brief);
+    }
+    public function select(){
+        $briefs = task::all();
+        return response()->json($briefs);
+    }
+
+    public function destroy($id){
+        $brief = task::find($id);
+        $brief->delete();
+        return response()->json($brief);
+    }
+
+    public function edit($id){
+        $brief = task::find($id);
+        return response()->json($brief);
+    }
+
+    public function update(Request $request,$id){
+        $brief = task::find($id);
+        $brief->firstname = $request->firstname;
+        $brief->lastname = $request->lastname;
+        $brief->email = $request->email;
+        $brief->save();
+        return $brief;
+    }
+}
---
+++ standard-tp/frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <App />
+    <h1>hhhhhhhh</h1>
+  </div>
+);
+
+reportWebVitals();
